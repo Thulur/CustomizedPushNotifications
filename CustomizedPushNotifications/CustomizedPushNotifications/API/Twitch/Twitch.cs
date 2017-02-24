3c0e5bce66e53fa0bb6b4f197b51456f352a450c
Sebastian Koall <koall.sebastian@gmail.com>
2017-02-24 21:51:16 +0100

    Implement notifications checking twitch streamers online state

--- CustomizedPushNotifications/CustomizedPushNotifications/API/Twitch/Twitch.cs
+++ CustomizedPushNotifications/CustomizedPushNotifications/API/Twitch/Twitch.cs
@@ -1,24 +1,51 @@
 ﻿using Newtonsoft.Json;
 using RestSharp;
+using System.Collections.Generic;
 
 namespace CustomizedPushNotifications.API.Twitch
 {
     class Twitch
     {
+        private static RestClient restClient = new RestClient("https://api.twitch.tv/kraken");
+
         public static bool StreamerOnline(string streamerName)
         {
-            var client = new RestClient("https://api.twitch.tv/kraken");
-
             var request = new RestRequest("streams/{streamerName}", Method.GET);
             request.AddUrlSegment("streamerName", streamerName);
 
             request.AddHeader("Accept", "application/vnd.twitchtv.v3+json");
             request.AddHeader("Client-ID", new Configuration().TwitchClientId);
 
-            IRestResponse response = client.Execute(request);
-            dynamic deserializedResponse = JsonConvert.DeserializeObject<dynamic>(response.Content);
+            IRestResponse response = restClient.Execute(request);
+            dynamic responseJson = JsonConvert.DeserializeObject<dynamic>(response.Content);
 
-            return deserializedResponse.stream != null;
+            return responseJson.stream != null;
+        }
+
+        public static Dictionary<string, bool> StreamersOnline(string[] streamerNames)
+        {
+            var result = new Dictionary<string, bool>();
+
+            foreach (var streamerName in streamerNames)
+            {
+                result.Add(streamerName, false);
+            }
+
+            var request = new RestRequest("streams?channel={streamerNames}", Method.GET);
+            request.AddUrlSegment("streamerNames", string.Join(",", streamerNames));
+
+            request.AddHeader("Accept", "application/vnd.twitchtv.v3+json");
+            request.AddHeader("Client-ID", new Configuration().TwitchClientId);
+
+            IRestResponse response = restClient.Execute(request);
+            dynamic responseJson = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+            foreach (dynamic stream in responseJson.streams)
+            {
+                result[stream.channel.name.Value] = true;
+            }
+
+            return result;
         }
     }
 }
--- CustomizedPushNotifications/CustomizedPushNotifications/MainActivity.cs
+++ CustomizedPushNotifications/CustomizedPushNotifications/MainActivity.cs
@@ -1,32 +1,50 @@
 ﻿using Android.App;
-using Android.Widget;
-using Android.OS;
 using Android.Content;
-using System;
-using CustomizedPushNotifications.API.Twitch;
+using Android.OS;
+using Android.Support.V4.App;
 using Android.Views;
+using Android.Widget;
 
-//using CustomizedPushNotifications.API.Twitch;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using CustomizedPushNotifications.API.Twitch;
+
+using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
 
 namespace CustomizedPushNotifications
 {
     [Activity(Label = "Customized Push Notifications", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private static readonly ApiService service = new ApiService();
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
-            var listView1 = FindViewById<ListView>(Resource.Id.listView1);
-            string[] items = { "laraloft: " + Twitch.StreamerOnline("laraloft"), "gronkh: " + Twitch.StreamerOnline("gronkh") };
-            listView1.Adapter = new ArrayAdapter<string>(this, Resource.Layout.Main, Android.Resource.Id.Text1, items);
+
+            FindViewById<Switch>(Resource.Id.activeSwitch).CheckedChange += SwitchToggled;
+        }
+
+        private void SwitchToggled(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            if (e.IsChecked)
+            {
+                ApplicationContext.StartService(new Intent(this, typeof(ApiService)));
+            }
+            else
+            {
+                ApplicationContext.StopService(new Intent(this, typeof(ApiService)));
+            }
         }
     }
 
-    /*public class StringListViewAdapter : BaseAdapter<string>
+    public class StringListViewAdapter : BaseAdapter<string>
     {
-        string[] items;
-        Activity context;
+        private string[] items;
+        private Activity context;
 
         public StringListViewAdapter(Activity context, string[] items) : base()
         {
@@ -59,30 +77,80 @@ namespace CustomizedPushNotifications
             view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = items[position];
             return view;
         }
-    }*/
+    }
 
 
 
-    /*[Service]
+    [Service(Name="thulur.CustomizedPushNotifications.ApiService", Exported=true, Process= ":ServiceProcess")]
     public class ApiService : Service
     {
-        public override IBinder OnBind(Intent intent)
+        private static readonly int ButtonClickNotificationId = 1000;
+        private static readonly int TimerWait = 30000;
+        private Timer timer;
+        private DateTime startTime;
+        private bool isStarted = false;
+
+        public override void OnCreate()
         {
-            throw new NotImplementedException();
+            base.OnCreate();
         }
 
-        public override StartCommandResult OnStartCommand(Android.Content.Intent intent, StartCommandFlags flags, int startId)
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
         {
-            var t = new Thread(() => {
-                Log.Debug("DemoService", "Doing work");
-                Thread.Sleep(5000);
-                Log.Debug("DemoService", "Work complete");
-                StopSelf();
+            if (!isStarted)
+            {
+                startTime = DateTime.UtcNow;
+                timer = new Timer(HandleTimerCallback, startTime, 0, TimerWait);
+                isStarted = true;
             }
-            );
-            t.Start();
+
             return StartCommandResult.Sticky;
         }
-    }*/
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+
+        public override void OnDestroy()
+        {
+            timer.Dispose();
+            timer = null;
+            isStarted = false;
+
+            base.OnDestroy();
+        }
+
+        void HandleTimerCallback(object state)
+        {
+            var streamersOnline = Twitch.StreamersOnline(new string[] { "laraloft", "gronkh" });
+
+            foreach (var entry in streamersOnline)
+            {
+                if (!entry.Value) continue;
+
+                Bundle valuesForActivity = new Bundle();
+                Intent resultIntent = new Intent(this, typeof(MainActivity));
+                resultIntent.PutExtras(valuesForActivity);
+
+                TaskStackBuilder stackBuilder = TaskStackBuilder.Create(this);
+                stackBuilder.AddParentStack(Java.Lang.Class.FromType(typeof(MainActivity)));
+                stackBuilder.AddNextIntent(resultIntent);
+
+                PendingIntent resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                    .SetAutoCancel(true)
+                    .SetContentIntent(resultPendingIntent)
+                    .SetContentTitle("Streamer online")
+                    .SetSmallIcon(Resource.Drawable.Icon)
+                    .SetContentText(entry.Key + " is streaming now");
+
+                NotificationManager notificationManager = (NotificationManager)GetSystemService(NotificationService);
+                notificationManager.Notify(ButtonClickNotificationId, builder.Build());
+            }
+        }
+    }
 }
 
